d0b80f21f7e4cf0b664f3e09ca93849272deb320
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-02-23 08:22:08 +0330

    create strucure of project

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import CardList from "@/components/casrdList/CardList";
+import CategoryList from "@/components/categoryList/CategoryList";
+import Featured from "@/components/featured/Featured";
+import Menu from "@/components/menu/Menu";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Featured />
+      <CategoryList />
+      <div className="">
+        <CardList />
+        <Menu />
+      </div>
+    </main>
+  );
+}
---
+++ components/casrdList/CardList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Pagination from "../pagination/Pagination";
+
+export default function CardList() {
+  return <div>CardList
+    <Pagination/>
+  </div>;
+}
